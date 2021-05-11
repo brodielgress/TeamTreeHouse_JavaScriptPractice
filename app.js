7a8a0649d0cd3c1ac554e029ec7cdcc5394576dd
Brodie Gress <77457950+brodielgress@users.noreply.github.com>
2021-05-11 14:30:23 -0400

    document selector practice finished

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
-const myTextInput = document.getElementById('myTextInput');
+// const myHeading = document.getElementsByTagName('h1')[0];
+// const myButton = document.getElementById('myButton');
+// const myTextInput = document.getElementById('myTextInput');
 
-myButton.addEventListener('click', () => {
-    myHeading.style.color = myTextInput.value;
-});
+// myButton.addEventListener('click', () => {
+//     myHeading.style.color = myTextInput.value;
+// });
 
-myTextInput.addEventListener('keypress', function (e) {
-    myHeading.style.color = myTextInput.value;
-})
+// myTextInput.addEventListener('keypress', function (e) {
+//     myHeading.style.color = myTextInput.value;
+// })
+
+const myList = document.getElementsByTagName('li');
+
+for (let i = 0; i < myList.length; i += 1) {
+    myList[i].style.color = 'purple';
+}
+
+const errorNotPurple = document.querySelectorAll('.error-not-purple');
+
+for (let i = 0; i < errorNotPurple.length; i += 1) {
+    errorNotPurple[i].style.color = 'red';
+}
+
+const evens = document.querySelectorAll('li:nth-child(odd)');
+
+for (let i = 0; i < errorNotPurple.length; i += 1) {
+    evens[i].style.backgroundColor = 'lightgray';
+}
